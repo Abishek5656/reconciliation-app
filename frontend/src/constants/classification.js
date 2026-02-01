e4d1752dcc1d7e0cb72b72b0f54c08ac9c06b1df
abishekvarma <abishek@entrolabs.com>
2026-02-01 14:30:07 +0530

    constant file

---
+++ frontend/src/constants/classification.js
@@ -0,0 +1,5 @@
+export const CLASSIFICATION_LABELS = {
+  5: "Present in Both",
+  6: "Present in Settlement But Not Partner Statement",
+  7: "Not Present in Settlement But Present in Partner Statement",
+};
