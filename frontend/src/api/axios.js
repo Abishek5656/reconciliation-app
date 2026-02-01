17d9c2a3260e48534ebaf67063ecae306d77082d
abishekvarma <abishek@entrolabs.com>
2026-02-01 15:04:19 +0530

    backend api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://reconciliation-app-y9sb.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
